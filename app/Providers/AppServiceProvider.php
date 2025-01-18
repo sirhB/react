14dde84a09d5f04d3a059cc5231835da7355d88e
Pushpak Chhajed <pushpak1300@gmail.com>
2025-01-18 19:36:06 +0530

    fix: use gemini models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -137,21 +137,21 @@ final class AppServiceProvider extends ServiceProvider
         // This is example of how to register a Prism.
         PrismServer::register(
             'Larasonic Small',
-            fn (): PendingRequest => Prism::text()->using(PrismProvider::Gemini, 'llama3-8b-8192')
+            fn (): PendingRequest => Prism::text()->using(PrismProvider::Gemini, 'gemini-1.5-flash')
                 ->withSystemPrompt(view('prompts.system')->render())
                 ->withMaxTokens(100)
         );
 
         PrismServer::register(
             'Larasonic Medium',
-            fn (): PendingRequest => Prism::text()->using(PrismProvider::Gemini, 'llama3-8b-8192')
+            fn (): PendingRequest => Prism::text()->using(PrismProvider::Gemini, 'gemini-1.5-flash')
                 ->withSystemPrompt(view('prompts.system')->render())
                 ->withMaxTokens(150)
         );
 
         PrismServer::register(
             'Larasonic Large',
-            fn (): PendingRequest => Prism::text()->using(PrismProvider::Gemini, 'llama3-8b-8192')
+            fn (): PendingRequest => Prism::text()->using(PrismProvider::Gemini, 'gemini-1.5-flash')
                 ->withSystemPrompt(view('prompts.system')->render())
                 ->withMaxTokens(250)
         );
